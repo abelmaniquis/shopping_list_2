a152be964aed6a8dfdb44244085bd9007205ca2d
abelmaniquis <abelmaniquis@gmail.com>
2016-06-27 20:33:50 +0000

    server methods added, trying to fix glitchy UI

--- server.js
+++ server.js
@@ -1,60 +1,86 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
 var app = express();
+var bodyParser = require('body-parser');
 
-var Storage = function(){
-  this.items = [];
-  this.id = 0;
+app.use(express.static('public'));
+
+app.use(bodyParser());
+
+
+/*
+
+  I want you to decouple all the function as I did in the app.delete
+  I want you to TRY figure out why is our UI not updating when we click on delete (x button)
+
+ */
+
+
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    //console.log(name);
+    var item = {
+        name: name,
+        id: this.id
+    };
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+Storage.prototype.appAdd = function(req,res){
+  //console.log(storage.items);
+  res.json(storage.items);
+}
+
+Storage.prototype.put = function(req,res){
+  storage.items[req.body.id].name = req.body.name;
+  res.json(storage.items);
+};
+
+Storage.prototype.post = function(req, res){
+  if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
 };
 
 
-Storage.prototype.add = function(name){
-  console.log(name);
-  var item = {name: name, id: this.id};
-  this.items.push(item);
-  this.id += 1;
-  return item;
+Storage.prototype.delete = function(req, res) {
+    storage.items.splice(req.params.id, 1);
+    res.status(202);
 };
-Storage.prototype.delete = function(name){
-  this.id -=1;
-};
+
+
 
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
+
+
 /*
 The app object uses the express.static middleware
 which tells express to serve any static content
 contained in the public folder
 */
 
-app.use(express.static('public'));
 
-app.get('/items',function(req,res){
-  res.json(storage.items);
-});
 
-app.post('/items',jsonParser,function(req,res){
-  if(!req.body){
-    return res.sendStatus(400);
-  }
+app.get('/items', storage.appAdd);
 
-  var item = storage.add(req.body.name);
-  res.status(201).json(item);
-});
+app.post('/items', storage.post);
 
-app.put('/items/:id',function(req,res){
-  console.log(storage.items);
-  res.json(storage.items);
-});
+app.put('/items/:id', storage.put);
 
-app.delete('/items/:id',function(req,res){
-  console.log(req)
-});
+app.delete('/items/:id', storage.delete);
 
 
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3005);
